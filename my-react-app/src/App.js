edde4b6cdc7f30069b9b81f085f1375709a69bb7
Gabriel <gesquivel@student.unsw.edu.au>
2024-05-17 17:24:34 +1000

    Basic react app

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a basic React app deployed on GitHub Pages.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
