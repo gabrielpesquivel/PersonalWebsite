fec8a0dfe35cb1008c8f5505fc59b03b7c61a18e
Gabriel Esquivel <z5358503@ad.unsw.edu.au>
2025-08-12 14:17:17 +1000

    Update experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './Experience.css';
-import mercuryImage from './Images/mercury.jpeg'; // Path to your image
-import sunswiftImage from './Images/sunswift.png'; // Path to your image
-import unswImage from './Images/unsw.jpeg'; // Path to your image
-import transgridImage from './Images/transgrid.png'; // Path to your image
-import EYImage from './Images/EY.png'; // Path to your image
-import OutlierImage from './Images/Outlier.png'; // Path to your image
+import mercuryImage from './Images/mercury.jpeg';
+import sunswiftImage from './Images/sunswift.png';
+import EYImage from './Images/EY.png';
+import NextCoinImage from './Images/NextCoin.jpg';
 
 function Experience() {
   return (
     <section id="experience" className="experience">
       <h2>Experience</h2>
+        <div className="experience-item">
+        <img src={NextCoinImage} alt="NC" className="experience-image" />
+        <div className="experience-content-wrapper">
+          <div className="experience-content">
+            <h3>Lead Software Engineer </h3>
+            <p>September 2025 – Current</p>
+            <p>NextCoin, UNSW Founders 10x Program</p>
+            <ul>
+              <li>Leading the end-to-end development of the platform, overseeing a team of engineers across the fullstack.</li>
+              <li>Architecting and building a scalable solution with machine learning powered matching.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <div className="experience-item">
         <img src={EYImage} alt="EY" className="experience-image" />
         <div className="experience-content-wrapper">
@@ -19,38 +31,7 @@ function Experience() {
             <p>November 2024 – January 2025</p>
             <p>Ernst & Young (EY), George St</p>
             <ul>
-              <li>Selected as an incoming participant to EY’s summer vacationer program in the technology consulting stream.</li>
-              <li>Role objective to conduct data analysis and develop machine learning models to support business decisions and
-              optimize operations.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="experience-item">
-        <img src={OutlierImage} alt="Outlier" className="experience-image" />
-        <div className="experience-content-wrapper">
-          <div className="experience-content">
-            <h3>AI Software Dataset Developer </h3>
-            <p>October 2024 – Current</p>
-            <p>Outlier, Remote</p>
-            <ul>
-              <li>In this role I develop and refine datasets for training AI models with a focus on software code generation and optimization.</li>
-              <li>In doing so, I improve AI-generated code through dataset adjustments, enhancing model performance for programming tasks.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="experience-item">
-        <img src={transgridImage} alt="Transgrid" className="experience-image" />
-        <div className="experience-content-wrapper">
-          <div className="experience-content">
-            <h3>Work Integrated Learning Program</h3>
-            <p>September 2024 – November 2024</p>
-            <p>Transgrid, Haymarket</p>
-            <ul>
-              <li>In a team of four students completing a complex consulting task as a part of the Work Integrated Learning Program at UNSW.</li>
-              <li>Project objective to find and evaluate methods and technologies with the potential to improve operability at
-              Transgrid in support of the challenges wrought by the energy transition.</li>
+              <li>Developed a cloud-hosted AI tool to increase efficiency of an insurance companies call center operations by up to 30%, tackling Natural Language Processing and text analytics challenges.</li>
             </ul>
           </div>
         </div>
@@ -63,8 +44,8 @@ function Experience() {
             <p>January 2024 – Present</p>
             <p>Sunswift Racing UNSW, Kensington</p>
             <ul>
-              <li>Developing software for all sensors and electronic devices to improve the performance and reliability of a multi-world-record holding solar car.</li>
-              <li>Collaborating with an interdisciplinary team of 75 people to push the boundaries of solar vehicle technology.</li>
+              <li>Developed software for onboard sensors and embedded electronic devices to improve the performance and reliability of a multi-world-record holding solar car. </li>
+              <li> Collaborated with an interdisciplinary team of 75 people to push the boundaries of solar vehicle technology. </li>
             </ul>
           </div>
         </div>
@@ -77,25 +58,8 @@ function Experience() {
             <p>November 2021 – August 2024</p>
             <p>Mercury Innovation Pty Ltd, Smithfield, NSW</p>
             <ul>
-              <li>Designed and modelled functional solutions in SOLIDWORKS to hardware issues.</li>
-              <li>Created insightful reports identifying problems with the company's latest digital transport signage solutions.</li>
-              <li>Assisted in assembly of digital transport signage to develop thorough understanding of the product and manufacturing processes.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="experience-item">
-        <img src={unswImage} alt="UNSW Faculty of Engineering Student Society" className="experience-image" />
-        <div className="experience-content-wrapper">
-          <div className="experience-content">
-            <h3>Executive</h3>
-            <p>March 2021 – December 2023</p>
-            <p>UNSW Faculty of Engineering Student Society, UNSW</p>
-            <ul>
-              <li>Produced high quality Careers-driven events for up to one hundred engineering student attendees.</li>
-              <li>Liaised with representatives from our sponsorship companies to develop mutually beneficial events.</li>
-              <li>Maintained compliance with university guidelines by liaising with UNSW Arc student society.</li>
-              <li>Handled room bookings and club grants applications for the society.</li>
+              <li>Designed functional solutions in Fusion & SOLIDWORKS to key robotics challenges faced within client projects. </li>
+              <li> Developed commuter-tracking capabilities on the next generation of digital parking signage</li>
             </ul>
           </div>
         </div>
