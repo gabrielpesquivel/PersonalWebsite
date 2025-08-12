61d6b8caf3aa579eac15cc9b1fc1b60679a33a87
Gabriel Esquivel <z5358503@ad.unsw.edu.au>
2025-08-12 14:19:21 +1000

    Update project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,12 +5,7 @@ function Projects() {
   return (
     <section id="projects" className="projects">
       <h2>Projects</h2>
-      <p> All repositories for my personal projects can be found on my github linked below! Otherwise, please reach out to me for my portfolio document.</p>
-      <ul>
-        <li>1. Early start-up looking to connect young athletes with private sports coaches in the Sydney region. </li>
-        <li>2. Hand-tracking software in Python (openCV and media pipe) to undergo a number of actions depending on the position of the hand</li>
-        <li>3. and many more!</li>
-      </ul>
+      <p> This section is in progress! Please check back soon. </p>
     </section>
   );
 }
