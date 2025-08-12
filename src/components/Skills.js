193d2bff31f78196792f9e880b8b392dcde976ec
Gabriel Esquivel <z5358503@ad.unsw.edu.au>
2025-08-12 14:20:49 +1000

    Update skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,9 +6,9 @@ function Skills() {
     <section id="skills" className="skills">
       <h2>Technical Skills</h2>
       <div className="skills-container">
-        <div className="skill-box">Languages: C, C++, Java, HTML, MIPS, Python, MATLAB</div>
-        <div className="skill-box">CAD Tools: SOLIDWORKS, Fusion360</div>
-        <div className="skill-box">Dev Tools: Arduino, Verilog, openCV, LaTeX, GitHub, GitLab</div>
+        <div className="skill-box">Languages: Python, C, C++, Java, JavaScript, Svelte, HTML, CSS, Assembly, Matlab</div>
+        <div className="skill-box">Frameworks: React, Flask, npm, Node.js, Django, Pytorch, Keras, OpenCV</div>
+        <div className="skill-box">DevOps & Tools: Docker, Git, Amazon Web Services (AWS), Azure, Google Cloud, WatsonX </div>
       </div>
     </section>
   );
