a5c0790c42d2b0b0b4f201e51f694c8e42285771
Gabriel Esquivel <z5358503@ad.unsw.edu.au>
2025-08-15 15:45:56 +1000

    Update Navbar aesthetics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,20 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
-        <li><RouterLink to="/" className="home-link">Home</RouterLink></li>
-        <li><Link to="about" smooth={true}>About</Link></li>
-        <li><Link to="experience" smooth={true}>Experience</Link></li>
-        <li><Link to="projects" smooth={true}>Projects</Link></li>
-        <li><Link to="skills" smooth={true}>Skills</Link></li>
-        <li><Link to="contact" smooth={true}>Contact</Link></li>
-        <li><RouterLink to="/blog" className="blog-link">Blog</RouterLink></li>
-      </ul>
+      <div className="navbar-name">
+        <RouterLink to="/" className="home-link">Gabriel Esquivel</RouterLink>
+      </div>
+      <div className="navbar-links">
+        <ul>
+          <li><Link to="about" smooth={true}>About</Link></li>
+          <li><Link to="experience" smooth={true}>Experience</Link></li>
+          <li><Link to="projects" smooth={true}>Projects</Link></li>
+          <li><Link to="skills" smooth={true}>Skills</Link></li>
+          <li><Link to="contact" smooth={true}>Contact</Link></li>
+          <li><RouterLink to="/blog" className="blog-link">Blog</RouterLink></li>
+        </ul>
+      </div>
+      <div class="navbar-placeholder"></div>
     </nav>
   );
 }
