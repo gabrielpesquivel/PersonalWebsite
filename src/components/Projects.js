b57a5e20371ef345272261be3615470ccb7e8e0c
Gabriel Esquivel <gabedoesstuff@gmail.com>
2025-10-11 11:22:22 +1100

    Update projects wording

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <section id="projects" className="projects">
       <h2>Projects</h2>
-      <p> This section is in progress! Please check back soon. </p>
+      <p> This section is currently under construction... </p>
     </section>
   );
 }
