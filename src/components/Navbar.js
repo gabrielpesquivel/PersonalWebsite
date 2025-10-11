51adfb77c7140fcee75a07c08924d9a007d93eca
Gabriel Esquivel <gabedoesstuff@gmail.com>
2025-10-11 12:31:27 +1100

    Mobile app reactivity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import { Link as RouterLink } from 'react-router-dom';
-import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope, FaBars, FaTimes } from 'react-icons/fa';
 import { SiLeetcode } from 'react-icons/si';
 import './Navbar.css';
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-name">
-        <RouterLink to="/" className="home-link">Gabriel Esquivel</RouterLink>
+        <RouterLink to="/" className="home-link" onClick={closeMenu}>Gabriel Esquivel</RouterLink>
       </div>
-      <div className="navbar-links">
+
+      <button className="hamburger" onClick={toggleMenu} aria-label="Toggle menu">
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </button>
+
+      <div className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
         <ul>
-          <li><Link to="about" smooth={true}>About</Link></li>
-          <li><Link to="experience" smooth={true}>Experience</Link></li>
-          <li><Link to="projects" smooth={true}>Projects</Link></li>
-          <li><Link to="skills" smooth={true}>Skills</Link></li>
-          <li><RouterLink to="/blog" className="blog-link">Blog</RouterLink></li>
+          <li><Link to="about" smooth={true} onClick={closeMenu}>About</Link></li>
+          <li><Link to="experience" smooth={true} onClick={closeMenu}>Experience</Link></li>
+          <li><Link to="projects" smooth={true} onClick={closeMenu}>Projects</Link></li>
+          <li><Link to="skills" smooth={true} onClick={closeMenu}>Skills</Link></li>
+          <li><RouterLink to="/blog" className="blog-link" onClick={closeMenu}>Blog</RouterLink></li>
         </ul>
       </div>
-      <div className="navbar-icons">
+      <div className={`navbar-icons ${isMenuOpen ? 'active' : ''}`}>
         <a href="mailto:gabrielpawluesquivel@gmail.com" target="_blank" rel="noopener noreferrer" aria-label="Email">
           <FaEnvelope />
         </a>
